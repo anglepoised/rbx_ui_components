0dab30be9d7f7d8ac3b28dbe472bbd9a86ab1b5a
Darwin Lopez <darwin.lopez@rockabox.com>
2015-04-14 11:09:57 +0100

    Updates demos

--- src/rb-page-header/demo/index.js
+++ src/rb-page-header/demo/index.js
@@ -2,13 +2,15 @@ define([
     '../index.js',
     './demo-ctrl',
     './demo-state',
+    '../../rb-header',
     '../../rb-nav-bar',
     '../../rb-icon',
     '../../rb-user'
-], function (rbPageHeader, demoCtrl, demoState, rbNavBar, rbIcon, rbUser) {
+], function (rbPageHeader, demoCtrl, demoState, rbHeader, rbNavBar, rbIcon, rbUser) {
     var demo = angular
         .module('rb-page-header-demo', [
             rbPageHeader.name,
+            rbHeader.name,
             rbNavBar.name,
             rbIcon.name,
             rbUser.name
