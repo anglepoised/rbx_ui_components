69e2d66ce34fcd9cdad9dd2db7247d4278a58a0a
Darwin Lopez <darwin.lopez@rockabox.com>
2015-04-21 14:01:47 +0100

    Moves warningStates from app.js to demo/index.js

--- src/rb-warning-messages/demo/index.js
+++ src/rb-warning-messages/demo/index.js
@@ -8,6 +8,17 @@ define([
             rbWarningMessages.name
         ])
         .config(demoState)
+        .value('warningStates', {
+            // A map of all available WARNING_IDENTIFIER: UI_ROUTER_STATE
+            // (key:value) pairs
+            // Everytime a user clicks an issue link on any warning message,
+            // will be a redirection to a UI-Router state.
+            // UI-Router states will be determined by this key:value map
+            // with key: WARNING_IDENTIFIER
+            // STATES FOR DEMO PURPOSES ONLY
+            'PARTIAL_TRACKING': 'tracking',
+            'OTHER_STATE': 'other_state'
+        })
         .controller('demo-warning-messages-ctrl', demoCtrl);
 
     return demo;
