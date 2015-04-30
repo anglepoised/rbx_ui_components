6e0ce3c81796fb706c3925a2c57eef2dfb943b69
Nick White <nick.white@rockabox.com>
2015-04-30 16:06:53 +0100

    Add angular-elastic to the textarea

--- src/rb-text-control/index.js
+++ src/rb-text-control/index.js
@@ -1,8 +1,9 @@
 define([
     './rb-text-control-directive',
     './custom-validation-directive',
+    'angular-elastic',
     './rb-text-control.css'
-], function (rbTextControlDirective, customValidationDirective, css) {
+], function (rbTextControlDirective, customValidationDirective, ngElastic, css) {
     /**
      * @ngdoc module
      * @name rb-text-control
@@ -12,7 +13,9 @@ define([
      *
      */
     var rbTextControl = angular
-        .module('rb-text-control', [])
+        .module('rb-text-control', [
+            'monospaced.elastic'
+        ])
         .directive('customValidation', customValidationDirective)
         .directive('rbTextControl', rbTextControlDirective);
 
