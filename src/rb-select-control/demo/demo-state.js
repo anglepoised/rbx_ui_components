73c7da924e5360f7a950ca3724945298f49182f1
Paul Love <paul@anglepoised.com>
2015-05-20 16:37:46 +0100

    Fix demo

--- src/rb-select-control/demo/demo-state.js
+++ src/rb-select-control/demo/demo-state.js
@@ -6,7 +6,7 @@ define([
     function demoState ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('rb-select-control', {
             url: '/rb-select-control',
-            controller: 'demo-rb-select-control-ctrl as demoCtrl',
+            controller: 'demo-rb-select-control-ctrl as demoRbSelectControlCtrl',
             template: template
         });
     }
