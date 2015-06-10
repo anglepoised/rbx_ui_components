18d27e4450f13156132887b2bedfce3cfc9d9ecb
jonman <jon.man@rockabox.com>
2015-06-10 17:26:15 +0100

    Refactor rb-nav-bar unit spec.

--- test/unit/rb-nav-bar/rb-nav-bar.spec.js
+++ test/unit/rb-nav-bar/rb-nav-bar.spec.js
@@ -3,77 +3,73 @@ define([
 ], function (rbNavBar) {
     describe('rb-nav-bar', function () {
 
-        var $scope,
-            $compile,
+        var $compile,
+            $scope,
             _options = [
                 {'text': 'Option 1'},
                 {'text': 'Option 2', 'state': 'test-state'},
                 {'text': 'Option 3', 'url': 'http://example.com', 'target': '_self'}
             ],
+            compileTemplate,
+            isolateScope,
             template = '<rb-nav-bar options="options"></rb-nav-bar>';
 
-        beforeEach(angular.mock.module(rbNavBar.name));
+        beforeEach(function () {
+            angular.mock.module(rbNavBar.name);
 
-        beforeEach(inject(function (_$compile_, _$rootScope_) {
-            $scope = _$rootScope_.$new({});
-            $compile = _$compile_;
-            $scope.options = _options;
-        }));
+            inject(function (_$compile_, _$rootScope_) {
+                $scope = _$rootScope_.$new({});
+                $compile = _$compile_;
+
+                compileTemplate = function (tpl) {
+                    $scope.options = _options;
+                    element = $compile(tpl)($scope);
+                    $scope.$apply();
+                    isolateScope = element.isolateScope();
+                };
+            });
+        });
 
         it('should convert attributes on a rb-nav-bar to attributes on the generated nav bar',
             function () {
-                var navBar = $compile('<rb-nav-bar anyattr any-attr></rb-nav-bar>')($scope);
+                compileTemplate('<rb-nav-bar anyattr any-attr></rb-nav-bar>');
 
-                $scope.$apply();
-                expect(navBar[0].hasAttribute('anyattr')).toBe(true);
-                expect(navBar[0].hasAttribute('any-attr')).toBe(true);
+                expect(element[0].hasAttribute('anyattr')).toBe(true);
+                expect(element[0].hasAttribute('any-attr')).toBe(true);
             });
 
         describe('rendering', function () {
 
             it('should render with a "nav" tagname', function () {
-                var navBar = angular.element(template),
-                    element = $compile(navBar)($scope);
+                compileTemplate(template);
 
-                $scope.$apply();
                 expect(element[0].tagName.toLowerCase()).toEqual('nav');
             });
 
             it('should place navigation options in the navigation bar', function () {
-                var navBar = angular.element(template),
-                    element = $compile(navBar)($scope);
+                compileTemplate(template);
 
-                $scope.$apply();
                 expect(element.find('a').length).toBe(3);
             });
 
             it('should set ui-sref if option has a state', function () {
-                var navBar = angular.element(template),
-                    element = $compile(navBar)($scope),
-                    options;
+                compileTemplate(template);
 
-                $scope.$apply();
-                options = angular.element(element.find('a'));
+                var options = angular.element(element.find('a'));
                 expect(angular.element(options[1]).attr('ui-sref')).toBe('test-state');
             });
 
             it('should set href if option has a url', function () {
-                var navBar = angular.element(template),
-                    element = $compile(navBar)($scope),
-                    options;
+                compileTemplate(template);
 
-                $scope.$apply();
-                options = angular.element(element.find('a'));
+                var options = angular.element(element.find('a'));
                 expect(angular.element(options[2]).attr('href')).toBe('http://example.com');
             });
 
             it('should set the target if set', function () {
-                var navBar = angular.element(template),
-                    element = $compile(navBar)($scope),
-                    options;
+                compileTemplate(template);
 
-                $scope.$apply();
-                options = angular.element(element.find('a'));
+                var options = angular.element(element.find('a'));
                 expect(angular.element(options[2]).attr('target')).toBe('_self');
             });
         });
