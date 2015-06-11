2e1a376e6e4b7124f8c78b801beb5f40cc5551b5
Chris Villa <chrisvilla@me.com>
2015-06-11 18:05:16 +0100

    Tests for `rb-grid-cell` sizing

--- test/unit/rb-grid/rb-grid-cell.spec.js
+++ test/unit/rb-grid/rb-grid-cell.spec.js
@@ -25,6 +25,13 @@ define([
             };
         }));
 
+        describe('isolated scope', function () {
+            it('should accept the expected attributes', function () {
+                compileTemplate('<rb-grid-cell size="1of2"></rb-grid-cell>');
+                expect(isolatedScope.size).toEqual('1of2');
+            });
+        });
+
         describe('compiled template', function () {
             it('should render with the expected SUIT class', function () {
                 compileTemplate('<rb-grid-cell></rb-grid-cell>');
@@ -35,6 +42,39 @@ define([
                 compileTemplate('<rb-grid-cell>TEST</rb-grid-cell>');
                 expect(element.text()).toBe('TEST');
             });
+
+            describe('size `attr`', function () {
+                it('should set the correct helper class for size=`1of2`', function () {
+                    compileTemplate('<rb-grid-cell size="1of2"></rb-grid-cell>');
+                    expect(element.hasClass('u-size1of2')).toBe(true);
+                });
+
+                it('should set the correct helper class for size=`1of3`', function () {
+                    compileTemplate('<rb-grid-cell size="1of3"></rb-grid-cell>');
+                    expect(element.hasClass('u-size1of3')).toBe(true);
+                });
+
+                it('should set the correct helper class for size=`2of3`', function () {
+                    compileTemplate('<rb-grid-cell size="2of3"></rb-grid-cell>');
+                    expect(element.hasClass('u-size2of3')).toBe(true);
+                });
+
+                it('should set the correct helper class for size=`1of4`', function () {
+                    compileTemplate('<rb-grid-cell size="1of4"></rb-grid-cell>');
+                    expect(element.hasClass('u-size1of4')).toBe(true);
+                });
+
+                it('should set the correct helper class for size=`3of4`', function () {
+                    compileTemplate('<rb-grid-cell size="3of4"></rb-grid-cell>');
+                    expect(element.hasClass('u-size3of4')).toBe(true);
+                });
+
+                it('should not set a helper class for other input', function () {
+                    compileTemplate('<rb-grid-cell size="6of7"></rb-grid-cell>');
+                    expect(element.hasClass('u-size6of7')).not.toBe(true);
+                });
+            });
+
         });
 
     });
