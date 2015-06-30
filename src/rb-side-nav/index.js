405ad41be2043bee8cb6d219bc70dabf213ad353
Chris Villa <chrisvilla@me.com>
2015-06-30 15:57:36 +0100

    Integrate `rb-action-bar` into `rb-side-bar`

--- src/rb-side-nav/index.js
+++ src/rb-side-nav/index.js
@@ -1,10 +1,11 @@
 define([
+    'components/rb-action-bar',
     'components/rb-button',
     'components/rb-icon',
     './rb-side-nav-directive',
     './rb-side-nav-item-directive',
     './rb-side-nav.css'
-], function (rbButton, rbIcon, rbSideNavDirective, rbSideNavItemDirective, css) {
+], function (rbActionBar, rbButton, rbIcon, rbSideNavDirective, rbSideNavItemDirective, css) {
     /**
      * @ngdoc module
      * @name rb-side-nav
@@ -14,7 +15,7 @@ define([
      *
      */
     var rbSideNav = angular
-        .module('rb-side-nav', [rbButton.name, rbIcon.name])
+        .module('rb-side-nav', [rbActionBar.name, rbButton.name, rbIcon.name])
         .directive('rbSideNav', rbSideNavDirective)
         .directive('rbSideNavItem', rbSideNavItemDirective);
 
