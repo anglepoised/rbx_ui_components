cf73bc3bf9dc41fed834b4252a2c33db213a630f
Darwin Lopez <darwin.lopez@rockabox.com>
2015-07-10 10:56:43 +0100

    Add a URL variant for the text control directive

--- src/rb-text-control/custom-validation-directive.js
+++ src/rb-text-control/custom-validation-directive.js
@@ -1,7 +1,9 @@
 define([
 ], function () {
     function customValidationDirective () {
-        var EMAIL_REGEXP = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        var EMAIL_REGEXP = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
+        DJANGO_URL = '^((?:http|ftp)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\\.)+(?:[A-Z]{2,6}\\.?|' +
+        '[A-Z0-9-]{2,}\\.?)|localhost|\\d{1,3}\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(?::\\d+)?(?:\/?|[\/?]\\S+)$';
 
         return {
             require: 'ngModel',
@@ -10,6 +12,12 @@ define([
 
                     ctrl.$validators.email = function (modelValue, viewValue) {
                         var result = ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+                        return result;
+                    };
+                } else if (scope.type === 'url') {
+
+                    ctrl.$validators.url = function (modelValue, viewValue) {
+                        var result = ctrl.$isEmpty(modelValue) || new RegExp(DJANGO_URL, 'i').test(modelValue);
                         return result;
                     };
                 } else if (scope.type === 'currency') {
