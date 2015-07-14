52ffa8b2c6064ce0e5d8ac20b4691f158f2d3fc1
Paul Love <paul@anglepoised.com>
2015-07-14 10:08:58 +0100

    Add `raw-html-display` component boilerplate

---
+++ src/rb-raw-html-display/rb-raw-html-display-directive.js
@@ -0,0 +1,34 @@
+define([
+    'html!./rb-raw-html-display.tpl.html'
+], function (template) {
+
+    /**
+     * @ngdoc directive
+     * @name rbRawHtmlDisplay
+     * @module rb-raw-html-display
+     *
+     * @restrict E
+     *
+     * @description
+     *
+     * @usage
+     * <hljs lang="html">
+     *    <rb-raw-html-display>
+     *     </rb-raw-html-display>
+     * </hljs>
+     *
+     * @ngInject
+     */
+    function rbRawHtmlDisplayDirective () {
+
+        return {
+            scope: {
+            },
+            restrict: 'E',
+            replace: true,
+            template: template
+        };
+    }
+
+    return rbRawHtmlDisplayDirective;
+});
---
+++ test/unit/rb-raw-html-display/rb-raw-html-display.spec.js
@@ -0,0 +1,35 @@
+define([
+    'components/rb-raw-html-display'
+], function (rbRawHtmlDisplay) {
+    describe('rb-raw-html-display', function () {
+
+        var $rootScope,
+            $scope,
+            isolatedScope,
+            $compile,
+            element,
+            compileTemplate;
+
+        beforeEach(angular.mock.module(rbRawHtmlDisplay.name));
+
+        beforeEach(inject(function (_$compile_, _$rootScope_) {
+            $rootScope = _$rootScope_;
+            $scope = _$rootScope_.$new({});
+            $compile = _$compile_;
+
+            // Compile directive, apply scope and fetch new isolated scope
+            compileTemplate = function (template) {
+                element = $compile(template)($scope);
+                $scope.$apply();
+                isolatedScope = element.isolateScope();
+            };
+        }));
+
+        describe('', function () {
+            it('should ', function () {
+                compileTemplate('<rb-raw-html-display></rb-raw-html-display>');
+            });
+        });
+
+    });
+});
