05d96d57117a6bf690f299fd8074832fc2e5db72
Jessie Shi <jessie.shi@me.com>
2015-07-22 18:12:28 +0100

    New Validation on too many digits

--- src/rb-text-control/custom-validation-directive.js
+++ src/rb-text-control/custom-validation-directive.js
@@ -28,9 +28,22 @@ define([
                             splitValue = (viewValue) ? viewValue.split('.') : [],
                             decimalsPlaces = (splitValue[1]) ? splitValue[1].length : 0,
                             result = ctrl.$isEmpty(modelValue) || (decimalsPlaces <= maxDecimals);
-
                         return result;
                     };
+
+                    ctrl.$validators.tooManyDigits = function (modelValue, viewValue) {
+                        var wholeNumberDigits, result;
+                        if (!modelValue) {
+                            wholeNumberDigits = 0;
+                        } else if (modelValue.toString().split('e+').length > 1) {
+                            wholeNumberDigits = modelValue.toString().split('e+')[1];
+                        } else {
+                            wholeNumberDigits = modelValue.toString().split('.')[0].length;
+                        }
+                        result = ctrl.$isEmpty(modelValue) || (wholeNumberDigits <= 18);
+                        return result;
+                    };
+
                 }
             }
         };
