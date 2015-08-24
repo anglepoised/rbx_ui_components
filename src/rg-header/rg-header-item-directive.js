b98dbc54add807569d1b5a55ab090af10a6a7baa
Paul Love <paul@anglepoised.com>
2015-08-24 12:28:41 +0100

    Add `rg-header-item` boilerplate

---
+++ src/rg-header/rg-header-item-directive.js
@@ -0,0 +1,35 @@
+define([
+    'html!./rg-header-item.tpl.html'
+], function (template) {
+
+    /**
+     * @ngdoc directive
+     * @name rgHeaderItem
+     * @module rg-header-item
+     *
+     * @restrict E
+     *
+     * @description
+     *
+     * @usage
+     * <hljs lang="html">
+     *    <rg-header-item>
+     *     </rg-header-item>
+     * </hljs>
+     *
+     * @ngInject
+     */
+    function rgHeaderItemDirective () {
+
+        return {
+            scope: {
+            },
+            restrict: 'E',
+            replace: true,
+            template: template,
+            transclude: true
+        };
+    }
+
+    return rgHeaderItemDirective;
+});
