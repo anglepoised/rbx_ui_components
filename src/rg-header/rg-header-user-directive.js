680cc1926553e868337764e2d292992b23c79d9f
Chris Villa <chrisvilla@me.com>
2015-08-25 16:22:49 +0100

    Copy `rb-user` to `rg-header-user` descendent
    Copied like-for-like, except for class renaming from `User*` to
    `RgHeader-user*`.

---
+++ src/rg-header/rg-header-user-directive.js
@@ -0,0 +1,35 @@
+define([
+    'html!./rg-header-user.tpl.html'
+], function (template) {
+
+    /**
+     * @ngdoc directive
+     * @name rgHeaderuser
+     * @module rg-header-user
+     *
+     * @restrict E
+     *
+     * @description
+     *
+     * @usage
+     * <hljs lang="html">
+     *    <rg-header-user>
+     *     </rg-header-user>
+     * </hljs>
+     *
+     * @ngInject
+     */
+    function rgHeaderUserDirective () {
+
+        return {
+            scope: {
+            },
+            restrict: 'E',
+            replace: true,
+            template: template,
+            transclude: true
+        };
+    }
+
+    return rgHeaderUserDirective;
+});
