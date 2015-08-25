2e932bbb4d744dd7f9d78741c8de2b5d2ebeeee8
Jessie Shi <jessie.shi@me.com>
2015-08-25 11:43:44 +0100

    Add custom validators to rg-text-control

--- src/rg-text-control/custom-validation-directive.js
+++ src/rg-text-control/custom-validation-directive.js
@@ -1,11 +1,26 @@
 define([
 ], function () {
     function customValidationDirective () {
+        var EMAIL_REGEXP = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
+        DJANGO_URL = '^((?:http|ftp)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\\.)+(?:[A-Z]{2,6}\\.?|' +
+        '[A-Z0-9-]{2,}\\.?)|localhost|\\d{1,3}\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(?::\\d+)?(?:\/?|[\/?]\\S+)$';
 
         return {
             require: 'ngModel',
             link: function (scope, elm, attrs, ctrl) {
-                if (scope.type === 'currency') {
+                if (attrs.type === 'email') {
+
+                    ctrl.$validators.email = function (modelValue, viewValue) {
+                        var result = ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+                        return result;
+                    };
+                } else if (attrs.type === 'url') {
+
+                    ctrl.$validators.url = function (modelValue, viewValue) {
+                        var result = ctrl.$isEmpty(modelValue) || new RegExp(DJANGO_URL, 'i').test(modelValue);
+                        return result;
+                    };
+                } else if (attrs.type === 'currency') {
 
                     ctrl.$validators.currency = function (modelValue, viewValue) {
                         var splitSteps = (scope.numberSteps) ? scope.numberSteps.toString().split('.') : [],
