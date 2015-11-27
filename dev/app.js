49e7c911b6e60ba87807097f27afbfe552951509
Jessie Shi <jessie.shi@me.com>
2015-11-27 16:26:40 +0000

    Test md theme

--- dev/app.js
+++ dev/app.js
@@ -180,7 +180,7 @@ define([
             mdToolbarDemo.name,
             mdDialogDemo.name
         ])
-        .config(function ($stateProvider, $httpProvider, $mdThemingProvider) {
+        .config(function ($stateProvider, $httpProvider) {
 
             // App State
             $stateProvider.state({
@@ -194,44 +194,6 @@ define([
                     $scope.states.shift();
                 }
             });
-
-            $mdThemingProvider.definePalette('scootaGreen', {
-                '50': '#3EB980',
-                '100': '#3EB980',
-                '200': '#3EB980',
-                '300': '#3EB980',
-                '400': '#3EB980',
-                '500': '#3EB980',
-                '600': '#3EB980',
-                '700': '#3EB980',
-                '800': '#3EB980',
-                '900': '#3EB980',
-                'A100': '#3EB980',
-                'A200': '#3EB980',
-                'A400': '#3EB980',
-                'A700': '#3EB980'
-            });
-
-            $mdThemingProvider.definePalette('scootaBlue', {
-                '50': '#068DCB',
-                '100': '#068DCB',
-                '200': '#068DCB',
-                '300': '#068DCB',
-                '400': '#068DCB',
-                '500': '#068DCB',
-                '600': '#068DCB',
-                '700': '#068DCB',
-                '800': '#068DCB',
-                '900': '#068DCB',
-                'A100': '#068DCB',
-                'A200': '#068DCB',
-                'A400': '#068DCB',
-                'A700': '#068DCB'
-            });
-
-            $mdThemingProvider.theme('default')
-                .primaryPalette('scootaBlue')
-                .accentPalette('scootaGreen');
 
         })
         .config(function ($urlRouterProvider, $locationProvider) {
